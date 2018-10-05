2d89a57ad63ecd39b884f4317e8230f6012c644f
David Wilding <david.wilding@zuora.com>
2018-10-05 09:08:21 +0800

    Set the title and description of the Postman collection

--- convert-zuora-swagger.js
+++ convert-zuora-swagger.js
@@ -16,10 +16,15 @@ refParser.dereference(specURL, {}, (err, spec) => {
   var specVersion = spec.info.version;
   console.log(`Swagger spec version is ${specVersion}`);
   
-  console.log("Converting Swagger spec to a Postman collection...");
+  // Prepare the spec
   delete spec.definitions;
+  spec.info.title = `Zuora REST API (${specVersion})`;
+  spec.info.description = "See https://www.zuora.com/developer/api-reference/ for the latest REST API documentation.";
+  
+  console.log("Converting Swagger spec to a Postman collection...");
   var postman = swagger2Postman.convert(spec);
   
+  // Save the Postman collection
   var postmanFilename = `zuora-postman-${specVersion}.json`;
   var postmanJSON = JSON.stringify(postman.collection, null, 2);
   fs.writeFileSync(postmanFilename, postmanJSON, "utf8");
