16a4055a78a0d84138b63e68040735fd88348a85
David Wilding <david.wilding@zuora.com>
2018-10-05 10:09:04 +0800

    Define a variable for the host

--- convert-zuora-swagger.js
+++ convert-zuora-swagger.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const refParser = require("json-schema-ref-parser");
 const swagger2Postman = require("./index.js");
+const PostmanVariableList = require('postman-collection').VariableList
 
-
+    
 var specURL = "https://www.zuora.com/wp-content/themes/zuora/yaml/swagger.yaml";
 
 console.log(`Downloading Swagger spec...`);
@@ -16,17 +17,27 @@ refParser.dereference(specURL, {}, (err, spec) => {
   var specVersion = spec.info.version;
   console.log(`Swagger spec version is ${specVersion}`);
   
-  // Prepare the spec
+  // Prepare the Swagger spec
   delete spec.definitions;
   spec.info.title = `Zuora REST API (${specVersion})`;
   spec.info.description = "See https://www.zuora.com/developer/api-reference/ for the latest REST API documentation.";
+  var defaultHost = spec.host;
+  spec.host = "{{zuora_host}}";
   
+  // Convert the Swagger spec
   console.log("Converting Swagger spec to a Postman collection...");
-  var postman = swagger2Postman.convert(spec);
+  var converted = swagger2Postman.convert(spec);
+  
+  // Define a variable for the host
+  converted.collection.variables = new PostmanVariableList(null, [{
+    key: "zuora_host",
+    value: defaultHost,
+    type: "string"
+  }]);
   
   // Save the Postman collection
   var postmanFilename = `zuora-postman-${specVersion}.json`;
-  var postmanJSON = JSON.stringify(postman.collection, null, 2);
+  var postmanJSON = JSON.stringify(converted.collection.toJSON(), null, 2);
   fs.writeFileSync(postmanFilename, postmanJSON, "utf8");
   console.log(`Saved Postman collection as ${postmanFilename}`);
   
