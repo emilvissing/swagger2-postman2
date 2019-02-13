a414b99a36dd679553dc0ff85d654ae23ff4f63f
David Wilding <david.wilding@zuora.com>
2019-02-13 10:16:11 +0800

    Updated Swagger spec URL, per https://community.zuora.com/t5/Developers/Zuora-s-Swagger-spec-has-a-new-URL/gpm-p/26944

--- convert-zuora-swagger.js
+++ convert-zuora-swagger.js
@@ -3,7 +3,7 @@ const refParser = require("json-schema-ref-parser");
 const swagger2Postman = require("./index.js");
 
     
-var specURL = "https://www.zuora.com/wp-content/themes/zuora/yaml/swagger.yaml";
+var specURL = "https://assets.zuora.com/zuora-documentation/swagger.yaml";
 
 console.log(`Downloading Swagger spec...`);
 refParser.dereference(specURL, {}, (err, spec) => {
